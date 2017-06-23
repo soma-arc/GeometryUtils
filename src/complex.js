625b1290d1ea553df9d92fc1ab9c4937bad9a0b6
soma_arc <somaarcr@gmail.com>
2017-06-23 17:51:34 +0900

    Remove assertion for type checking and add JSDoc
    We can check type with jsdoc-to-assert

--- src/complex.js
+++ src/complex.js
@@ -1,36 +1,53 @@
-import assert from 'power-assert';
-
 const SQRT2 = Math.sqrt(2);
 const EPSILON = 0.0000001;
 
 export default class Complex {
+    /**
+     *
+     * @param {number} re
+     * @param {number} im
+     */
     constructor(re, im) {
-        assert.ok(typeof re === 'number');
-        assert.ok(typeof im === 'number');
         this.re = re;
         this.im = im;
     }
 
+    /**
+     *
+     * @param {Complex} c
+     * @returns {Complex}
+     */
     add(c) {
-        assert.ok(c instanceof Complex);
         return new Complex(this.re + c.re,
                            this.im + c.im);
     }
 
+    /**
+     *
+     * @param {Complex} c
+     * @returns {Complex}
+     */
     sub(c) {
-        assert.ok(c instanceof Complex);
         return new Complex(this.re - c.re,
                            this.im - c.im);
     }
 
+    /**
+     *
+     * @param {Complex} c
+     * @returns {Complex}
+     */
     mult(c) {
-        assert.ok(c instanceof Complex);
         return new Complex((this.re * c.re) - (this.im * c.im),
                            (this.re * c.im) + (this.im * c.re));
     }
 
+    /**
+     *
+     * @param {Complex} c
+     * @returns {Complex}
+     */
     div(c) {
-        assert.ok(c instanceof Complex);
         if (c.isInfinity()) {
             return Complex.ZERO;
         }
@@ -43,93 +60,177 @@ export default class Complex {
                            (this.im * c.re - this.re * c.im) / denom);
     }
 
+    /**
+     *
+     * @param {Complex} c1
+     * @param {Complex} c2
+     * @returns {Complex}
+     */
     static sum (c1, c2) {
-        assert.ok(c1 instanceof Complex);
         return c1.add(c2);
     }
 
+    /**
+     *
+     * @param {Complex} c1
+     * @param {Complex} c2
+     * @returns {Complex}
+     */
     static diff (c1, c2) {
-        assert.ok(c1 instanceof Complex);
         return c1.sub(c2);
     }
 
+    /**
+     *
+     * @param {Complex} c1
+     * @param {Complex} c2
+     * @returns {Complex}
+     */
     static prod (c1, c2) {
-        assert.ok(c1 instanceof Complex);
         return c1.mult(c2);
     }
 
+    /**
+     *
+     * @param {Complex} c1
+     * @param {Complex} c2
+     * @returns {Complex}
+     */
     static quot (c1, c2) {
-        assert.ok(c1 instanceof Complex);
         return c1.div(c2);
     }
 
+    /**
+     *
+     * @param {number} k
+     * @returns {Complex}
+     */
     scale(k) {
-        assert.ok(typeof k === 'number');
         return new Complex(this.re * k, this.im * k);
     }
 
+    /**
+     *
+     * @returns {number}
+     */
     arg () {
         return Math.atan2(this.im, this.re);
     }
 
+    /**
+     *
+     * @returns {Complex}
+     */
     conjugate () {
         return new Complex(this.re, -this.im);
     }
 
+    /**
+     *
+     * @param {Complex} c
+     * @returns {Complex}
+     */
     static conjugate (c) {
         return c.conjugate();
     }
 
+    /**
+     *
+     * @returns {Complex}
+     */
     normalize () {
         return this.scale(1 / this.abs());
     }
 
+    /**
+     *
+     * @returns {number}
+     */
     abs () {
         return Math.sqrt(this.re * this.re + this.im * this.im);
     }
 
+    /**
+     *
+     * @param {Complex} c
+     * @returns {number}
+     */
     static abs (c) {
         return c.abs();
     }
 
+    /**
+     *
+     * @returns {number}
+     */
     absSq() {
         return this.re * this.re + this.im * this.im;
     }
 
+    /**
+     *
+     * @param {Complex} c
+     * @returns {number}
+     */
     static absSq (c) {
         return c.absSq();
     }
 
+    /**
+     *
+     * @param {Complex} c
+     * @returns {boolean}
+     */
     eq(c) {
-        assert.ok(c instanceof Complex);
         const re = this.re - c.re;
         const im = this.im - c.im;
         return (re * re + im * im) < EPSILON;
     }
 
+    /**
+     *
+     * @param {Complex} c1
+     * @param {Complex} c2
+     * @returns {boolean}
+     */
     static eq(c1, c2) {
-        assert.ok(c1 instanceof Complex);
-        assert.ok(c2 instanceof Complex);
         const re = c1.re - c2.re;
         const im = c1.im - c2.im;
         return (re * re + im * im) < EPSILON;
     }
 
+    /**
+     *
+     * @param {Complex} c1
+     * @param {Complex} c2
+     * @returns {number}
+     */
     static distance(c1, c2) {
-        assert.ok(c1 instanceof Complex);
-        assert.ok(c2 instanceof Complex);
         return c1.sub(c2).abs();
     }
 
+    /**
+     *
+     * @returns {Complex}
+     */
     sq () {
         return new Complex((this.re * this.re) - (this.im * this.im),
                            (this.re * this.im) + (this.im * this.re));
     }
 
+    /**
+     *
+     * @param {Complex} c
+     * @returns {Complex}
+     */
     static sq(c) {
         return c.sq();
     }
 
+    /**
+     *
+     * @returns {Complex}
+     */
     sqrt () {
         if (this.im > 0) {
             return new Complex(Math.sqrt(this.re + Math.sqrt(this.re * this.re +
@@ -148,54 +249,109 @@ export default class Complex {
         return new Complex(Math.sqrt(this.re), 0);
     }
 
+    /**
+     *
+     * @param {Complex} c
+     * @returns {Complex}
+     */
     static sqrt(c) {
         return c.sqrt();
     }
 
+    /**
+     *
+     * @param {Complex} c1
+     * @param {Complex} c2
+     * @returns {number}
+     */
     static dot(c1, c2) {
         return c1.re * c2.re + c1.im * c2.im;
     }
 
+    /**
+     *
+     * @returns {boolean}
+     */
     isInfinity() {
         return (this.re === Number.POSITIVE_INFINITY || this.im === Number.POSITIVE_INFINITY);
     }
 
+    /**
+     *
+     * @returns {boolean}
+     */
     isZero() {
         return (Math.abs(this.re) < EPSILON && Math.abs(this.im) < EPSILON);
     }
 
+    /**
+     *
+     * @returns {boolean}
+     */
     isReal () {
         return Math.abs(this.im) < EPSILON;
     }
 
+    /**
+     *
+     * @returns {boolean}
+     */
     isPureImaginary () {
         return Math.abs(this.re) < EPSILON;
     }
 
+    /**
+     *
+     * @returns {boolean}
+     */
     hasNaN () {
         return isNaN(this.re) || isNaN(this.im);
     }
 
+    /**
+     *
+     * @returns {number[]}
+     */
     get linearArray () {
         return [this.re, this.im];
     }
 
+    /**
+     *
+     * @returns {Complex}
+     */
     static get ZERO() {
         return new Complex(0, 0);
     }
 
+    /**
+     *
+     * @returns {Complex}
+     */
     static get ONE() {
         return new Complex(1, 0);
     }
 
+    /**
+     *
+     * @returns {Complex}
+     */
     static get I() {
         return new Complex(0, 1);
     }
 
+    /**
+     *
+     * @returns {Complex}
+     */
     static get MINUS_ONE() {
         return new Complex(-1, 0);
     }
 
+    /**
+     *
+     * @returns {Complex}
+     */
     static get INFINITY() {
         return new Complex(Number.POSITIVE_INFINITY, Number.POSITIVE_INFINITY);
     }
--- src/sl2c.js
+++ src/sl2c.js
@@ -1,39 +1,58 @@
-import assert from 'power-assert'
 import Complex from './complex';
 
 export default class SL2C {
+    /**
+     *
+     * @param {Complex} a
+     * @param {Complex} b
+     * @param {Complex} c
+     * @param {Complex} d
+     */
     constructor (a, b, c, d) {
-        assert.ok(a instanceof Complex);
-        assert.ok(b instanceof Complex);
-        assert.ok(c instanceof Complex);
-        assert.ok(d instanceof Complex);
         this.a = a;
         this.b = b;
         this.c = c;
         this.d = d;
     }
 
+    /**
+     *
+     * @param {SL2C} m
+     * @returns {SL2C}
+     */
     mult (m) {
-        assert.ok(m instanceof SL2C);
         return new SL2C(this.a.mult(m.a).add(this.b.mult(m.c)),
                         this.a.mult(m.b).add(this.b.mult(m.d)),
                         this.c.mult(m.a).add(this.d.mult(m.c)),
                         this.c.mult(m.b).add(this.d.mult(m.d)));
     }
 
+    /**
+     *
+     * @param {SL2C} m1
+     * @param {SL2C} m2
+     * @returns {SL2C}
+     */
     static prod (m1, m2) {
-        assert.ok(m1 instanceof SL2C);
         return m1.mult(m2);
     }
 
+    /**
+     *
+     * @param {SL2C} m
+     * @returns {SL2C}
+     */
     conjugate (m) {
-        assert.ok(m instanceof SL2C);
         // m^-1 this m
         return SL2C.prod(SL2C.prod(m.inverse(), this), m);
     }
 
+    /**
+     *
+     * @param {Complex} c
+     * @returns {SL2C}
+     */
     apply (c) {
-        assert.ok(c instanceof Complex);
         if (c.isInfinity()) {
             if (!this.c.isZero()) {
                 return this.a.div(this.c);
@@ -51,35 +70,63 @@ export default class SL2C {
         }
     }
 
+    /**
+     *
+     * @returns {SL2C}
+     */
     determinant () {
         return this.a.mult(this.d).sub(this.b.mult(this.c));
     }
 
+    /**
+     *
+     * @param {Complex} k
+     * @returns {SL2C}
+     */
     scale (k) {
-        assert.ok(k instanceof Complex);
         return new SL2C(this.a.mult(k), this.b.mult(k),
                         this.c.mult(k), this.d.mult(k));
     }
 
+    /**
+     *
+     * @returns {SL2C}
+     */
     inverse () {
         return new SL2C(this.d, this.b.mult(Complex.MINUS_ONE),
                         this.c.mult(Complex.MINUS_ONE), this.a).scale(Complex.ONE.div(this.determinant()));
     }
 
+    /**
+     *
+     * @returns {Complex}
+     */
     trace () {
         return this.a.add(this.d);
     }
 
+    /**
+     *
+     * @returns {boolean}
+     */
     hasNaN () {
         return this.a.hasNaN() || this.b.hasNaN() || this.c.hasNaN() || this.d.hasNaN();
     }
 
+    /**
+     *
+     * @returns {number[]}
+     */
     get linearArray () {
         return this.a.linearArray.concat(this.b.linearArray,
                                          this.c.linearArray,
                                          this.d.linearArray);
     }
 
+    /**
+     *
+     * @returns {SL2C}
+     */
     static get UNIT () {
         return new SL2C(Complex.ONE, Complex.ZERO,
                         Complex.ZERO, Complex.ONE);
